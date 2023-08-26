5dcbb7d183df9227b74ae1f00fa0f5658f3562ea
licjosechavez <t10_chavez@hotmail.com>
2023-08-26 14:50:12 -0300

    Subida de proyecto

---
+++ app/views/inc/head.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title><?php echo APP_NAME; ?></title>
+  <!-- loader-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/pace.min.css" rel="stylesheet"/>
+  <script src="./assets/js/pace.min.js"></script>
+  <!--favicon-->
+  <link rel="icon" href="<?php echo APP_URL;?>app/views/inc/assets/images/favicon.ico" type="image/x-icon">
+  <!-- Vector CSS -->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+  <!-- simplebar CSS-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+  <!-- Bootstrap core CSS-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/bootstrap.min.css" rel="stylesheet"/>
+  <!-- animate CSS-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <!-- Icons CSS-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/icons.css" rel="stylesheet" type="text/css"/>
+  <!-- Sidebar CSS-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/sidebar-menu.css" rel="stylesheet"/>
+  <!-- Custom Style-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/app-style.css" rel="stylesheet"/>
+  <!-- Sweet Alert-->
+  <link href="<?php echo APP_URL;?>app/views/inc/assets/css/sweetalert2.min.css" rel="stylesheet"/>
+  <script src="<?php echo APP_URL;?>app/views/inc/assets/js/sweetalert2.all.min.js"></script>
+  
---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+
+    const APP_URL = "http://localhost/sistala/";
+    const APP_NAME = "SISTEMA TALA";
+    const APP_SESSION_NAME = "SISTALA";
+    date_default_timezone_set("America/Argentina/La_Rioja");
